e34a708ac9150130a3ce8a934e2b972665a8a71a
Ekaterina Podranyuk <katherineswork44@gmail.com>
2019-09-12 18:11:27 +0300

    reorganized test classes made helpers file to collect all test classes for orders and orderlines; also made these classes similiar to used in examples

---
+++ tests/helpers.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from pycdek import AbstractOrder, AbstractOrderLine
+
+
+class MyOrder(AbstractOrder):
+    """
+    Класс-наследник от AbstractOrder для тестирования
+    """
+    def __init__(self, id=1, lines=None, **kwargs):
+        self.id = id
+        self.lines = lines
+
+    # Список товаров в заказе
+    def get_products(self):
+        return self.lines
+
+
+class MyOrderLine(AbstractOrderLine):
+    """
+    Класс-наследник от AbstractOrderLine для тестирования
+    """
+    product = {
+        'title': 'Шлакоблок',
+        'weight': 1000,
+        'price': 500,
+        'id': 1
+    }
+
+    def __init__(self, order=None):
+        self.parent_order = order
+
+    def get_product_title(self):
+        return self.product['title']
+
+    def get_product_upc(self):
+        return self.product['id']
+
+    def get_product_weight(self):
+        return self.product['weight']
+
+    def get_product_price(self):
+        return self.product['price']
+
+    def get_product_payment(self):
+        if self.parent_order and self.parent_order.is_paid:
+            return 0
+        else:
+            return self.product['price']  # оплата при получении
